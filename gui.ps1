ce71ede0bb0f2a0991161e2f033e0500f556e378
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-09-11 00:17:29 -0300

    Limpar Diretorio

--- gui.ps1
+++
@@ -1,8 +0,0 @@
-    Add-Type -assembly System.Windows.Forms
-    $Form = New-Object System.Windows.Forms.Form
-    $Form.Text = "My PowerShell GUI"
-    $Label = New-Object System.Windows.Forms.Label
-    $Label.Text = "Hello from PowerShell!"
-    $Label.Location = New-Object System.Drawing.Point(50,50)
-    $Form.Controls.Add($Label)
-    $Form.ShowDialog()
--- limpador.bat
+++
@@ -1,6 +0,0 @@
-del /q/f/s %TEMP%\\
-rd /s /q C:\$Recycle.Bin
-rd /s /q %userprofile%\download
-rd /s /q %userprofile%\imagens
-mkdir %userprofile%\download
-mkdir %userprofile%\imagens
